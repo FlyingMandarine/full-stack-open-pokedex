b0eae8e754c81da85f73af8f3efdb1a4935b6a90
Patrice Hermenault <patrice@aptap.co.uk>
2022-11-27 12:57:17 +0000

    Exercise 11.12-12

--- health_check.sh
+++ health_check.sh
@@ -2,7 +2,15 @@
 
 echo "Hello from shell script"
 
+echo "Update 1"
+apt-get update
+echo "Update 2"
+apt install -y curl
+
+echo "curltest variable init"
 curltest=$(curl http://localhost:8080/health)
+
+echo "comparison"
 
 if [ $curltest == "ok" ]; then
   exit 0
